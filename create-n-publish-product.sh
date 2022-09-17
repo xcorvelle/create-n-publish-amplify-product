cf296503a2df4fa69753642b4652144f0cddd958
xcorvelle <xylinacorvelle@gmail.com>
2022-09-16 22:01:38 -0700

    Update create-n-publish-product.sh

--- create-n-publish-product.sh
+++ create-n-publish-product.sh
@@ -241,7 +241,7 @@ do
 done
 
 axway central create -f json_files/quota.json -y -o json > json_files/quota-created.json
-
+cat json_files/quota-created.json
 error_exit "Problem with creating Quota" "json_files/quota-created.json"
 
 
